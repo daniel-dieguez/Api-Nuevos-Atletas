c341420c99f060b6d831dc3a8d5ec21ecab1b314
daniel-dieguez <danguez2001@gmail.com^C>
2024-02-13 10:43:40 -0600

    He creado y agregado un post para el objeto de Atletas

--- src/main/java/com/atletas/atletas/controller/AtletasController.java
+++ src/main/java/com/atletas/atletas/controller/AtletasController.java
@@ -3,6 +3,7 @@ package com.atletas.atletas.controller;
 
 import com.atletas.atletas.dao.services.IAtletasServicesImpl;
 import com.atletas.atletas.models.model.Atletas;
+import jakarta.validation.Valid;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,13 +12,14 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.CannotCreateTransactionException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/atletas/v1/atletas")
@@ -45,6 +47,39 @@ public class AtletasController {
             response = this.getTransactionExepcion(response, e);
             return new ResponseEntity<Map<String, Object>>(response, HttpStatus.SERVICE_UNAVAILABLE);
         }catch(DataAccessException e){
+            response = this.getDataAccessException(response, e);
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.SERVICE_UNAVAILABLE);
+
+        }
+
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody Atletas value, BindingResult result){
+        Map<String, Object> response = new HashMap<>();
+        if(result.hasErrors() ==true){
+            List<String> errores = result.getFieldErrors().stream().map(error -> error.getDefaultMessage())
+                    .collect(Collectors.toList()); // getFielError me enlistara los campos que se encuentraron errores, El collector nos ayuda a enlistar lso errorres PREGUNTAR A CHATGPT
+            response.put("errores", errores);
+            logger.info("se encontraron errores al momento de validar");
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+        }
+        try{
+            Atletas atletas = new Atletas();
+            atletas.setId_atleta(UUID.randomUUID().toString());
+            atletas.setNombre(value.getNombre());
+            atletas.setEmail(value.getEmail());
+            atletas.setPassword(value.getPassword());
+            atletas.setPago(value.getPago());
+            logger.info("Se acaba de agregar nuevo atleta");
+            response.put("mensaje", "Un nuevo atleta se ingreso");
+            response.put("Atleta", atletas);
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+        }catch (CannotCreateTransactionException e){
+            response = this.getTransactionExepcion(response, e);
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.SERVICE_UNAVAILABLE);
+
+        }catch (DataAccessException e){
             response = this.getDataAccessException(response, e);
             return new ResponseEntity<Map<String, Object>>(response, HttpStatus.SERVICE_UNAVAILABLE);
 
